bdef94ea426de96acd041675a39b3b941e048bf2
At <atrisarkar91@gmail.com>
2022-01-05 20:54:06 +0530

    reorganized hikari_dev

--- crates/hikari_dev/src/profile.rs
+++ crates/hikari_dev/src/profile.rs
@@ -12,3 +12,49 @@ pub fn clean_function_name(name: &str) -> &str {
         name
     }
 }
+
+#[macro_export]
+macro_rules! profile_scope {
+    ($name: expr) => {
+        $crate::profiling::scope!($name);
+    };
+
+    ($name: expr, $data: expr) => {
+        $crate::profiling::scope!($name, $data);
+    }
+}
+
+#[macro_export]
+macro_rules! profile_function {
+    () => {
+        $crate::profile_scope!($crate::function!()) 
+    }
+}
+
+#[macro_export]
+macro_rules! finish_frame {
+    () => {
+        $crate::profiling::finish_frame!()
+    }
+}
+
+#[macro_export]
+macro_rules! function {
+    () => {
+        {
+            fn f() {}
+            fn type_name_of<T>(_: T) -> &'static str {
+                std::any::type_name::<T>()
+            }
+            let name = type_name_of(f);
+            let name = &name[..name.len() - 3];
+    
+            $crate::clean_function_name(name)
+         }
+    };
+}
+
+pub use profile_function;
+pub use profile_scope;
+pub use function;
+pub use finish_frame;
