6ce1368bdceacda8e706f48b8ca9c411b176dd7d
At <atrisarkar91@gmail.com>
2022-07-09 15:12:41 +0530

    Add default impls for Camera and Light

--- crates/hikari_3d/src/camera.rs
+++ crates/hikari_3d/src/camera.rs
@@ -6,11 +6,20 @@ pub struct Camera {
     pub projection: Projection,
 }
 
+impl Default for Camera {
+    fn default() -> Self {
+        Self { near: 0.1,
+        far: 1000.0,
+    exposure: 1.0,
+projection: Projection::Perspective(45.0) }
+    }
+}
 #[derive(Clone, Copy, Debug)]
 pub enum Projection {
     Perspective(f32), //fov in degrees
     Orthographic,
 }
+
 
 impl Camera {
     pub fn get_projection_matrix(&self, width: u32, height: u32) -> hikari_math::Mat4 {
--- crates/hikari_3d/src/light.rs
+++ crates/hikari_3d/src/light.rs
@@ -1,9 +1,19 @@
+use hikari_math::Vec4;
+
+
+
 #[derive(Clone, Copy, Debug)]
 pub struct Light {
-    pub color: hikari_math::Vec4,
+    pub color: Vec4,
     pub intensity: f32,
     pub cast_shadows: bool,
     pub kind: LightKind,
+}
+
+impl Default for Light {
+    fn default() -> Self {
+        Self { color: Vec4::ONE, intensity: 1.0, cast_shadows: true, kind: LightKind::Directional }
+    }
 }
 
 #[derive(Clone, Copy, Debug)]
