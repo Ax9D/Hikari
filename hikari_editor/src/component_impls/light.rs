cb41569362a71e4cac64d78c5ae1b7f4e7bab238
Atri Sarkar <atrisarkar91@gmail.com>
2022-12-16 16:14:07 +0530

    Add Shadow related settings in the Editor

--- hikari_editor/src/component_impls/light.rs
+++ hikari_editor/src/component_impls/light.rs
@@ -1,5 +1,5 @@
 use crate::{components::EditorComponent, *};
-use hikari::g3d::{Light, ShadowInfo, LightKind};
+use hikari::g3d::{Light, LightKind};
 use hikari_editor::*;
 
 impl EditorComponent for Light {
@@ -24,33 +24,36 @@ impl EditorComponent for Light {
         _editor: &mut Editor,
         _state: EngineState,
     ) -> anyhow::Result<()> {
-        imgui::ColorPicker4::new("color", &mut self.color)
-            .display_rgb(true)
-            .display_hex(true)
-            .build(ui);
+        // imgui::ColorPicker4::new("color", &mut self.color)
+        //     .display_rgb(true)
+        //     .display_hex(true)
+        //     .small_preview(true)
+        //     .build(ui);
+        imgui::ColorEdit4::new("Color", &mut self.color)
+        .picker(true)
+        .build(ui);
 
-        imgui::Drag::new("intensity").build(ui, &mut self.intensity);
+        imgui::Drag::new("Intensity").build(ui, &mut self.intensity);
+        ui.checkbox("Cast shadows", &mut self.shadow.enabled);
 
-        let mut shadows_enabled = self.shadow.is_some();
-
-        let changed = ui.checkbox("cast shadows", &mut shadows_enabled);
-        if changed {
-            if shadows_enabled {
-                self.shadow = Some(ShadowInfo::default());
-            } else {
-                self.shadow = None;
+        ui.combo("Type", &mut 0, &[LightKind::Directional], |kind| {
+            match kind {
+                LightKind::Point => std::borrow::Cow::Borrowed("Point"),
+                LightKind::Directional =>  std::borrow::Cow::Borrowed("Directional"),
             }
-        }
+        });
 
-        if let Some(shadow_info) = &mut self.shadow {
-            ui.input_float("Constant Bias", &mut shadow_info.constant_bias).build();
+        ui.enabled(self.shadow.enabled, || {
+            let shadow_info = &mut self.shadow;
+            ui.input_float("Sloped Scaled Bias", &mut shadow_info.slope_scaled_bias).build();
             imgui::Slider::new("Normal Bias", 0.0, 5.0).build(ui, &mut shadow_info.normal_bias);
             if self.kind == LightKind::Directional {
                 imgui::Slider::new("Cascade Split Lambda", 0.0, 1.0).build(ui, &mut shadow_info.cascade_split_lambda);
                 ui.input_float("Max Shadow Distance", &mut shadow_info.max_shadow_distance).build();
                 imgui::Slider::new("Shadow Fade", 0.0, 1.0).build(ui, &mut shadow_info.fade);
+                ui.checkbox("Cull Front Face", &mut shadow_info.cull_front_face);
             }
-        }
+        });
         Ok(())
     }
 
--- hikari_editor/src/editor/render_settings.rs
+++ hikari_editor/src/editor/render_settings.rs
@@ -1,7 +1,8 @@
 use hikari::g3d::ShaderLibrary;
+use hikari::g3d::ShaderLibraryConfig;
+use hikari::pbr::ShadowResolution;
 use hikari::pbr::WorldRenderer;
 use hikari::render::Gfx;
-use hikari::render::imgui_support::TextureExt;
 use hikari_editor::EngineState;
 
 use super::Editor;
@@ -14,29 +15,51 @@ pub fn draw(ui: &imgui::Ui, _editor: &mut Editor, state: EngineState) -> anyhow:
     .build(|| {
         let mut renderer = state.get_mut::<WorldRenderer>().unwrap();
         let mut gfx = state.get_mut::<Gfx>().unwrap();
+        let mut shader_lib = state.get_mut::<ShaderLibrary>().unwrap();
 
-        let setttings = renderer.settings();
+        renderer.update_settings(&mut gfx, &mut shader_lib, |settings| {
+            ui.checkbox("VSync", &mut settings.vsync);
+            ui.checkbox("FXAA", &mut settings.fxaa);
 
-        ui.checkbox("FXAA", &mut setttings.fxaa);
+            //FIX ME: Enum selection macro maybe?
+            let mut current_res = settings.directional_shadow_map_resolution as usize;
+            ui.combo("Directional Shadow Map Resolution", &mut current_res, 
+            &[
+                ShadowResolution::D256,
+                ShadowResolution::D512,
+                ShadowResolution::D1024,
+                ShadowResolution::D2048,
+                ShadowResolution::D4096,
+            ], |kind| {
+            match kind {
+                ShadowResolution::D256 => std::borrow::Cow::Borrowed("256"),
+                ShadowResolution::D512 => std::borrow::Cow::Borrowed("512"),
+                ShadowResolution::D1024 => std::borrow::Cow::Borrowed("1024"),
+                ShadowResolution::D2048 => std::borrow::Cow::Borrowed("2048"),
+                ShadowResolution::D4096 => std::borrow::Cow::Borrowed("4096"),
+            }
+            });
+
+            settings.directional_shadow_map_resolution = match current_res {
+                0 => ShadowResolution::D256,
+                1 => ShadowResolution::D512,
+                2 => ShadowResolution::D1024,
+                3 => ShadowResolution::D2048,
+                4 => ShadowResolution::D4096,
+                _=> unreachable!()
+
+            };
+            ui.separator();
+            
+            ui.checkbox("Show Shadow Cascades", &mut settings.debug.show_shadow_cascades);
+        }).expect("Failed to update settings");
+
         ui.separator();
-        if ui.button("Disable Vsync") {
-            gfx.set_vsync(false);
-        }
-
-        ui.checkbox("Show Shadow Cascades", &mut setttings.debug.show_shadow_cascades);
         
-        let shadow_map = renderer.graph_resources().get_image_by_name("DirectionalShadowMapDebug0").unwrap();
-
-        imgui::Image::new(ui.get_texture_id(shadow_map), [400.0, 400.0]).build(ui);
-        ui.same_line();
-
-        let depth_map = renderer.graph_resources().get_image_by_name("PrepassDepthDebug").unwrap();
-
         imgui::Image::new(ui.get_texture_id(depth_map), [400.0, 400.0]).build(ui);
 
-
+            
         if ui.button("Reload Shaders") {
-            let mut shader_lib = state.get_mut::<ShaderLibrary>().unwrap();
             match shader_lib.reload() {
                 Ok(_) => {},
                 Err(why) => log::error!("{}", why),
