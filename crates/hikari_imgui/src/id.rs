b770d25350b3ec60e3808b76906dc63485072965
Atri Sarkar <atrisarkar91@gmail.com>
2023-02-04 16:36:46 +0530

    Update Imgui

--- crates/hikari_imgui/src/id.rs
+++ crates/hikari_imgui/src/id.rs
@@ -8,6 +8,6 @@ pub trait IdCustomExt {
 
 impl IdCustomExt for Id {
     fn new<T: Hash>(data: &T, ui: &Ui) -> Id {
-        Id::Int(fxhash::hash(data) as i32, ui)
+        ui.new_id_int(fxhash::hash(data) as i32)
     }
 }
