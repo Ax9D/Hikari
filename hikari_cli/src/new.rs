fbe1be0fc177551e78fbc9fe86021db7d5cdae0d
Atri Sarkar <atrisarkar91@gmail.com>
2023-03-07 01:14:38 +0530

    Fix `hikari_cli` Don't copy templates dir because we don't have one anymore

--- hikari_cli/src/new.rs
+++ hikari_cli/src/new.rs
@@ -13,23 +13,15 @@ pub fn run(path: PathBuf) -> anyhow::Result<()> {
         .to_str()
         .ok_or(anyhow::anyhow!(
             "Project name must be a valid unicode string"
-        ))?
-        .to_owned();
-    let mut file_name_with_extension = PathBuf::from(file_name.clone());
+        ))?;
+        
+    let mut file_name_with_extension = PathBuf::from(&file_name);
     file_name_with_extension.set_extension(hikari_editor::PROJECT_EXTENSION);
     let file_path = path.join(file_name_with_extension);
 
     std::fs::create_dir(&path)?;
-    fs_extra::dir::copy(
-        "./templates/new_project",
-        &path,
-        &fs_extra::dir::CopyOptions {
-            content_only: true,
-            ..Default::default()
-        },
-    )?;
 
-    hikari_editor::Project::new(file_name.clone())
+    hikari_editor::Project::new(&file_name)
         .save(&file_path)
         .unwrap();
 
