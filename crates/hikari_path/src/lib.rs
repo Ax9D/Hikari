6c86000d42eb99b16813bb27c989207ecaa18ddd
Atri Sarkar <atrisarkar91@gmail.com>
2023-03-07 01:10:02 +0530

    Add `hikari_path` crate stub
    We need a custom path type to differentiate between engine and
    user assets. This crate will contain the implementation

---
+++ crates/hikari_path/src/lib.rs
@@ -0,0 +1,14 @@
+pub struct AssetPath {
+    inner: str
+}
+pub struct AssetPathBuf {
+    inner: String
+}
+
+impl AssetPathBuf {
+    pub fn new() -> Self {
+        Self {
+            inner: String::new()
+        }
+    }
+}
