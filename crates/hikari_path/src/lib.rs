137c4959cf99dc476eca57d8f8183665c51a7a50
Atri Sarkar <atrisarkar91@gmail.com>
2025-03-25 10:29:37 +0530

    Introduce hikari_path to replace rust Paths

--- crates/hikari_path/src/lib.rs
+++ crates/hikari_path/src/lib.rs
@@ -1,14 +1,50 @@
+use std::ops::Deref;
+
+pub const ENGINE_URI: &str = "engine://";
+pub const USER_URI: &str = "user://";
+pub const SEPARATOR: &str = "/";
+
 pub struct AssetPath {
     inner: str,
+}
+impl AssetPath {
+    pub fn new<S: AsRef<str> + ?Sized>(s: &S) -> &AssetPath {
+        unsafe { &*(s.as_ref() as *const str as *const AssetPath) }
+    }
 }
 pub struct AssetPathBuf {
     inner: String,
 }
 
 impl AssetPathBuf {
-    pub fn new() -> Self {
+    pub fn empty() -> Self {
         Self {
-            inner: String::new(),
+            inner: String::new()
         }
+    }
+    pub fn engine() -> Self {
+        Self {
+            inner: String::from(ENGINE_URI),
+        }
+    }
+    pub fn user() -> Self {
+        Self {
+            inner: String::from(USER_URI)
+        }
+    }
+    pub fn as_asset_path(&self) -> &AssetPath {
+        self
+    }
+    pub fn push<P: AsRef<AssetPath>>(&mut self, path: P) {
+        self.inner.push_str(&path.as_ref().inner);
+        self.inner.push_str(SEPARATOR);
+    }
+}
+
+impl Deref for AssetPathBuf {
+    type Target = AssetPath;
+
+    fn deref(&self) -> &Self::Target {
+        AssetPath::new(&self.inner)
     }
 }
